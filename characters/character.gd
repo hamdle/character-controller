a03b864975e880613d20378417269148ed3ebe40
hamdle <contact@ericmarty.co>
2018-05-25 23:01:10 -0700

    Add jump animation

--- characters/character.gd
+++ characters/character.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 
 var input_direction = Vector2()
-var last_move_direction = Vector2(1, 0)
 
 const MAX_WALK_SPEED = 450
 const MAX_RUN_SPEED = 700
@@ -12,8 +11,6 @@ var max_speed = 0
 var motion = Vector2()
 
 func _physics_process(delta):
-	if input_direction:
-		last_move_direction = input_direction
 	if input_direction:
 		if speed != max_speed:
 			speed = max_speed
