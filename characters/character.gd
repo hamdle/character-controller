c70521bf1d38caa6bbe2b99f5a66d58bdd4cc0de
hamdle <contact@ericmarty.co>
2018-05-25 22:30:07 -0700

    Add 4 directional player movement

--- characters/character.gd
+++ characters/character.gd
@@ -1,9 +1,24 @@
 extends KinematicBody2D
 
+var input_direction = Vector2()
+var last_move_direction = Vector2(1, 0)
 
-func _ready():
-	pass
+const MAX_WALK_SPEED = 450
+const MAX_RUN_SPEED = 700
 
+var speed = 0
+var max_speed = 0
+
+var motion = Vector2()
 
 func _physics_process(delta):
-	pass
+	if input_direction:
+		last_move_direction = input_direction
+	if input_direction:
+		if speed != max_speed:
+			speed = max_speed
+	else:
+		speed = 0
+
+	var velocity = input_direction.normalized() * speed
+	move_and_slide(velocity, Vector2(), 5, 2)
--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,5 +1,18 @@
 extends "res://characters/character.gd"
 
+signal direction_changed
 
 func _physics_process(delta):
-	pass
+	input_direction = Vector2()
+	
+	# 4 directions
+	if Input.is_action_pressed('move_up'):
+		input_direction.y = -1
+	elif Input.is_action_pressed('move_down'):
+		input_direction.y = 1
+	elif Input.is_action_pressed('move_right'):
+		input_direction.x = 1
+	elif Input.is_action_pressed('move_left'):
+		input_direction.x = -1
+
+	max_speed = MAX_RUN_SPEED if Input.is_action_pressed("run") else MAX_WALK_SPEED
